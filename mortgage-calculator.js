ba8de469479586c85e7730785b38ff0c766aa3f3
Ch Yogeesh Sai <115868784+Yogeeshsai@users.noreply.github.com>
2024-08-04 19:57:25 +0530

    Create mortgage-calculator.js

---
+++ mortgage-calculator.js
@@ -0,0 +1,10 @@
+import MortgageCalculator from '../components/MortgageCalculator';
+
+const MortgageCalculatorPage = () => (
+  <div>
+    <h1>Mortgage Calculator</h1>
+    <MortgageCalculator />
+  </div>
+);
+
+export default MortgageCalculatorPage;
